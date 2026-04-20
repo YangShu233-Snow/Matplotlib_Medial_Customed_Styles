a35ac10082dfaf31d48a8926014f739092a99b5d
Yangshu233 <750704076@qq.com>
2026-04-20 11:35:40 +0800

    update: single graph column chart

--- styles/single_graph_columns_chart/example.py
+++ styles/single_graph_columns_chart/example.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
 import numpy as np
 
 from pathlib import Path
@@ -13,13 +14,18 @@ def caculate_star_y_position(mean: int, sem: int):
     return mean + sem + sem * 0.05
 
 def draw_stars(ax: Axes, groups_id: List[int], stars: List[int], means, errs):
-    for group_id, star in zip(groups_id, stars):
-        index = group_id -1
+    for index, star in zip(groups_id, stars):
         star_y_position = caculate_star_y_position(means[index], errs[index])
 
-        ax.text(group_id, star_y_position, '*' * star,
+        ax.text(index, star_y_position, '*' * star,
                 ha='center', va='bottom')
 
+def generate_prism_colors(num_groups):
+    if num_groups == 1:
+        return ['black']
+
+    grays = np.linspace(0, 0.8, num_groups)
+    return [str(g) for g in grays]
 
 def main():
     # config
@@ -37,19 +43,21 @@ def main():
     x_pos = np.arange(len(groups))
 
     # 默认误差线仅作上半部分，若需要“工”字完整误差线，则asymmetric_errs = [errs] 
-    asymmetric_errs = [[0, 0], errs] 
+    asymmetric_errs = [[0] * len(groups), errs] 
 
     # 具体图像尺寸大小按需求设置
-    fig, ax = plt.subplots(figsize=(3, 4), dpi=300)
+    fig, ax = plt.subplots(figsize=(len(groups) * 1.5, 4), dpi=300)
+
+    colors = generate_prism_colors(len(groups))
 
     # 图表柱子的样式
     ax.bar(x_pos, means, yerr=asymmetric_errs, width=0.6,
-            color=['black', '#999999'], edgecolor='black', linewidth=2,
+            color=colors, edgecolor='black', linewidth=2,
             capsize=5, error_kw={'elinewidth': 0.8, 'capthick': 0.8})
     
-    draw_stars(ax, groups_id=[2], stars=3, means=means, errs=errs)
+    draw_stars(ax, groups_id=[1], stars=[3], means=means, errs=errs)
 
-    ax.set_xlim(-0.6, 1.6)
+    ax.set_xlim(-0.6, len(groups) - 1 + 0.6)
     ax.set_xticks(x_pos)
     ax.set_xticklabels(groups)
     ax.set_ylabel(ylabel)
